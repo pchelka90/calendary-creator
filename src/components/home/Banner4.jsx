35a1ecb0356de5b01cf1e663960a381a6a56d9b7
Bartek Bronkowski <84997081+pchelka90@users.noreply.github.com>
2022-04-06 01:53:59 +0200

    Add files via upload

---
+++ src/components/home/Banner4.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Banner4 = () => {
+  return (
+    <div className='banner4'>
+      <h1 className='banner4H1'>Kontakt</h1>
+    </div>
+  );
+};
+
+export default Banner4;

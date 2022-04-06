eebf4823fc209ce9c938609f18e3e9801b0610ac
Bartek Bronkowski <84997081+pchelka90@users.noreply.github.com>
2022-04-06 04:36:49 +0200

    Add files via upload

---
+++ src/components/home/Hero.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Kreator';
+
+const Hero = () => {
+  return (
+    <div className='columns'>
+      <div className='column'>
+        <div data-aos='fade-right' className='hero__content__container'>
+          <img className='heroIMG' src='/assets/images/Napis.svg' />
+        </div>
+      </div>
+      <div className='column'>
+        <div data-aos='fade-left' className='hero__slider__img__container'>
+          <img
+            src='/assets/images/Kreator.svg'
+            className='hero__slider__img'
+            alt='obrazek kreatora'
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/components/utilities/Photos.js
@@ -0,0 +1,47 @@
+export const photos = [
+  {
+    src: '/assets/images/3ksiazkowe.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: '/assets/images/2ksiazkowe.jpg',
+    width: 1,
+    height: 1,
+  },
+  {
+    src: '/assets/images/4ksiazkowe.jpg',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: '/assets/images/5ksiazkowe.jpg',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: '/assets/images/6ksiazkowe.jpg',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: '/assets/images/7ksiazkowe.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: '/assets/images/8ksiazkowe.jpg',
+    width: 3,
+    height: 4,
+  },
+  {
+    src: '/assets/images/9ksiazkowe.jpg',
+    width: 4,
+    height: 3,
+  },
+  {
+    src: '/assets/images/10ksiazkowe.jpg',
+    width: 4,
+    height: 3,
+  },
+];

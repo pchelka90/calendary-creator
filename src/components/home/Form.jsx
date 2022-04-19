8e6e7fb963e6703a175c33d7a4f90fbf47011cb1
Bartek <bartlomiejbronkowski90@gmail.com>
2022-04-19 13:19:16 +0200

    master

--- src/components/home/Form.jsx
+++ src/components/home/Form.jsx
@@ -72,165 +72,170 @@ class Form extends React.Component {
   render() {
     return (
       <div className='contact-box' id='kontakt'>
-        <div className='formularzyk'>
-          <h2 className='formH1'>
-            <img className='formIMG' src='/assets/images/list.svg' />
-            <strong>Prześlij swój projekt</strong>
-            <br />
-            za pomocą formularza
-          </h2>
-        </div>
-        <img className='strzaleczka' src='/assets/images/strzaleczka.png' />
-        <form
-          name='kontakt'
-          id='kontaktform'
-          method='post'
-          data-netlify='true'
-          data-netlify-honeypot='bot-field'
-          onSubmit={this.handleSubmit}
-          className='boxshadow'
-          style={{ backgroundColor: '#23C495' }}
-        >
-          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-          <input type='hidden' name='form-name' value='aplikacja' />
-          <div hidden>
-            <label>
-              Don’t fill this out:{' '}
-              <input name='bot-field' onChange={this.handleChange} />
-            </label>
-          </div>
-          <label className='label_imie' htmlFor={'name'}>
-            Imię i nazwisko<sup>*</sup>:
-          </label>
-          <div className='control'>
-            <input
-              className='input'
-              type={'text'}
-              name={'imię'}
-              onChange={this.handleChange}
-              id={'imionew'}
-              required={true}
-            />
-          </div>
-          <label className='label_email' htmlFor={'email'}>
-            Adres E-mail<sup>*</sup>:
-          </label>
-          <div className='control'>
-            <input
-              className='input'
-              type={'email'}
-              name={'adres email'}
-              onChange={this.handleChange}
-              id={'adres emailnew'}
-              required={true}
-            />
-          </div>
-          <div className='field numer_tel'>
-            <label className='label' htmlFor={'name'}>
-              Numer telefonu:
-            </label>
-            <div className='control'>
-              <input
-                className='input'
-                type={'number'}
-                name={'numer telefonu'}
-                onChange={this.handleChange}
-                id={'numertelnew'}
-                required={false}
-              />
-            </div>
-          </div>
-
-          <div className='control'>
-            <label className='label is-size-10'>Załącz plik:</label>
-            {/*załącz logo*/}
-
-            <div className='file'>
-              <label className='file-label' style={{ cursor: 'pointer' }}>
-                {/*onInput ={(o) => handleInput(o)}*/}
-                <input
-                  className='custom-file-input'
-                  type='file'
-                  name='plik'
-                  style={{ cursor: 'pointer' }}
-                  onChange={this.handleAttachment}
-                  id='fileinputnew'
-                />
+        <div className='columns'>
+          <div className='column contact'>
+            <h1 className='formH1'>
+              <img className='formIMG' src='/assets/images/list.svg' />
+              <strong>Prześlij swój projekt</strong>
+              <br />
+              za pomocą formularza
+            </h1>
+            <img className='strzaleczka' src='/assets/images/strzaleczka.png' />
+            <form
+              name='kontakt'
+              id='kontaktform'
+              method='post'
+              data-netlify='true'
+              data-netlify-honeypot='bot-field'
+              onSubmit={this.handleSubmit}
+              className='boxshadow'
+              style={{ backgroundColor: '#23C495' }}
+            >
+              {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+              <input type='hidden' name='form-name' value='aplikacja' />
+              <div hidden>
+                <label>
+                  Don’t fill this out:{' '}
+                  <input name='bot-field' onChange={this.handleChange} />
+                </label>
+              </div>
+              <label className='label_imie' htmlFor={'name'}>
+                Imię i nazwisko<sup>*</sup>:
               </label>
-            </div>
-            <br />
-            <p
-              align='center'
-              style={{
-                backgroundColor: '#111111',
-                color: 'white',
-                padding: '2px',
-                fontSize: '12px',
-              }}
-            >
-              {' '}
-              Maksymalna wielkość pliku to <b>1MB</b>.{' '}
-            </p>
-          </div>
-          <label className='label_wiadomosc' htmlFor={'message'}>
-            Wiadomość<sup>*</sup>:
-          </label>
-          <textarea
-            className='textarea'
-            type={'text'}
-            name={'wiadomość'}
-            onChange={this.handleChange}
-            id={'wiadomośćnew'}
-            required={true}
-            rows='7'
-          ></textarea>
-          <div className='checkbox'>
-            <label
-              style={{ fontSize: '12px' }}
-              className='main'
-              htmlFor='privacynew'
-            >
-              {' '}
-              <input
-                required={true}
+              <div className='control'>
+                <input
+                  className='input'
+                  type={'text'}
+                  name={'imię'}
+                  onChange={this.handleChange}
+                  id={'imionew'}
+                  required={true}
+                />
+              </div>
+              <label className='label_email' htmlFor={'email'}>
+                Adres E-mail<sup>*</sup>:
+              </label>
+              <div className='control'>
+                <input
+                  className='input'
+                  type={'email'}
+                  name={'adres email'}
+                  onChange={this.handleChange}
+                  id={'adres emailnew'}
+                  required={true}
+                />
+              </div>
+              <div className='field numer_tel'>
+                <label className='label' htmlFor={'name'}>
+                  Numer telefonu:
+                </label>
+                <div className='control'>
+                  <input
+                    className='input'
+                    type={'number'}
+                    name={'numer telefonu'}
+                    onChange={this.handleChange}
+                    id={'numertelnew'}
+                    required={false}
+                  />
+                </div>
+              </div>
+
+              <div className='control'>
+                <label className='label is-size-10'>Załącz plik:</label>
+                {/*załącz logo*/}
+
+                <div className='file'>
+                  <label className='file-label' style={{ cursor: 'pointer' }}>
+                    {/*onInput ={(o) => handleInput(o)}*/}
+                    <input
+                      className='custom-file-input'
+                      type='file'
+                      name='plik'
+                      style={{ cursor: 'pointer' }}
+                      onChange={this.handleAttachment}
+                      id='fileinputnew'
+                    />
+                  </label>
+                </div>
+                <br />
+                <p
+                  align='center'
+                  style={{
+                    backgroundColor: '#111111',
+                    color: 'white',
+                    padding: '2px',
+                    fontSize: '12px',
+                  }}
+                >
+                  {' '}
+                  Maksymalna wielkość pliku to <b>1MB</b>.{' '}
+                </p>
+              </div>
+              <label className='label_wiadomosc' htmlFor={'message'}>
+                Wiadomość<sup>*</sup>:
+              </label>
+              <textarea
+                className='textarea'
+                type={'text'}
+                name={'wiadomość'}
                 onChange={this.handleChange}
-                type='checkbox'
-                id='privacynew'
-                name='privacy'
-                defaultChecked='true'
-                value='true'
-              />
-              Wyrażam zgodę na przetwarzanie moich danych zgodnie z naszą{' '}
-              <a
-                className='link-green'
-                style={{ color: 'white' }}
-                href='/polityka-prywatnosci/'
-              >
-                polityką prywatności
-              </a>
-            </label>
-            <div className='button_wyslij'>
-              <button
-                className='button is-primary'
-                style={{ border: '1px solid white' }}
-                type='submit'
-                onSubmit={this.handleSubmit}
-                onClick={showFileSize}
-              >
-                Wyślij
-              </button>
+                id={'wiadomośćnew'}
+                required={true}
+                rows='7'
+              ></textarea>
+              <div className='checkbox'>
+                <label
+                  style={{ fontSize: '12px' }}
+                  className='main'
+                  htmlFor='privacynew'
+                >
+                  {' '}
+                  <input
+                    required={true}
+                    onChange={this.handleChange}
+                    type='checkbox'
+                    id='privacynew'
+                    name='privacy'
+                    defaultChecked='true'
+                    value='true'
+                  />
+                  Wyrażam zgodę na przetwarzanie moich danych zgodnie z naszą{' '}
+                  <a
+                    className='link-green'
+                    style={{ color: 'white' }}
+                    href='/polityka-prywatnosci/'
+                  >
+                    polityką prywatności
+                  </a>
+                </label>
+                <div className='button_wyslij'>
+                  <button
+                    className='button is-primary'
+                    style={{ border: '1px solid white' }}
+                    type='submit'
+                    onSubmit={this.handleSubmit}
+                    onClick={showFileSize}
+                  >
+                    Wyślij
+                  </button>
+                </div>
+              </div>
+            </form>
+            <div className='rest'>
+              <h1 className='restH1'>
+                <strong>Teraz odpocznij</strong>
+                <p className='filizanka'>
+                  <img className='restIMG' src='/assets/images/kawa.svg' />
+                </p>
+                <br />
+                my zajmiemy się resztą
+              </h1>
             </div>
           </div>
-        </form>
-        <div className='rest'>
-          <h1 className='restH1'>
-            <strong>Teraz odpocznij</strong>
-            <p className='filizanka'>
-              <img className='restIMG' src='/assets/images/kawa.svg' />
-            </p>
-            <br />
-            my zajmiemy się resztą
-          </h1>
+          <div className='column obrazek-kreator'>
+            <img src='/assets/images/Kreator.svg' />
+          </div>
         </div>
       </div>
     );

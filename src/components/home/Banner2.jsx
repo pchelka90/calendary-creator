7ef1e1d2b9d0abe504b9f8bc7d30357194c1fcac
ys-sudo <georgios.lazaridis0@gmail.com>
2022-04-19 16:22:36 +0200

    spinner + więcej tekstu

--- src/components/home/Banner2.jsx
+++ src/components/home/Banner2.jsx
@@ -111,6 +111,18 @@ const Banner2 = () => {
 
     <br />
     <br />
+
+
+
+    <div className="container" style={{paddingBottom:'75px'}}>
+    <h2 style={{textAlign:'left'}}>Lorem SEO</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <h2 style={{textAlign:'left'}}>Lorem SEO</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <h2 style={{textAlign:'left'}}>Lorem SEO</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+
     <br />
 
     <div className='banner2'>
--- src/components/utilities/Spinner.js
+++ src/components/utilities/Spinner.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Spinner = () => {
     return (
         <div style={{position:'absolute',top:'50%',left:'50%',transform:'translate(-50%,-50%)'}}>
-            <img src='assets/images/spinner.gif' alt='spinner' />
+            <img src='assets/images/spinner.gif' style={{filter:'hue-rotate(145deg) brightness(1.2)'}} alt='spinner' />
         </div>
     );
 };

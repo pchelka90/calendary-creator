c0c640084a72ddbe0c12911b686267c49c241292
Bartek <bartlomiejbronkowski90@gmail.com>
2022-05-02 00:20:40 +0200

    main

---
+++ build/static/js/720.a9da2a66.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkkreator_kalendarza=self.webpackChunkkreator_kalendarza||[]).push([[720],{2720:function(e,a,n){n.r(a);n(2791);var r=n(184);a.default=function(){return(0,r.jsxs)("div",{className:"banner3",children:[(0,r.jsx)("h2",{className:"banner3H1",children:"Czy wiesz \u017ce..."}),(0,r.jsxs)("p",{className:"banner3P",children:["... w naszej ofercie mo\u017cesz znale\u017a\u0107 du\u017co wi\u0119cej ni\u017c kalendarze czy notesy?",(0,r.jsx)("br",{}),"Poznaj obszary, w kt\xf3rych dzia\u0142amy."]})]})}}}]);
+//# sourceMappingURL=720.a9da2a66.chunk.js.map
